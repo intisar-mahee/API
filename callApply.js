8bec227fc966e13117989c2499ee5525565431ac
Intisar Mahee <67532512+intisar-mahee@users.noreply.github.com>
2020-09-12 13:22:03 +0600

    sec

--- callApply.js
+++ callApply.js
@@ -6,10 +6,28 @@ const normalPerson = {
         console.log(this.firstName, this.lastName);
     },
     chargeBill: function(amount){
+        console.log(this);
         this.salary= this.salary-amount;
         return this.salary;
     }
 }
 
-normalPerson.chargeBill(150);
+const heroPerson = {
+    firstName: 'Hero',
+    lastName: 'Balam',
+    salary: 25000,
+}
+
+const friendlyPerson = {
+    firstName: 'Hero',
+    lastName: 'kalam',
+    salary: 20000,
+}
+
+const heroBillCharge = normalPerson.chargeBill.bind(heroPerson);
+heroBillCharge(2000);
+heroBillCharge(3000);
+console.log(heroPerson.salary);
 console.log(normalPerson.salary);
+
+const friendlyChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
